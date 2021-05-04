91b428e16bce27ece69f5845879dd5e1f4a7d1b7
tna007 <anh.to@edu.bc.fi>
2021-05-04 19:49:47 +0300

    Add recipe from query to database

--- src/Controller/HomeScreen2Controller.php
+++ src/Controller/HomeScreen2Controller.php
@@ -9,27 +9,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeScreen2Controller extends AbstractController
 {
-//    #[Route('/home/screen', name: 'home_screen', methods: ['GET'])]
-//    public function index(Request $request): Response
-//    {
-//        return $this->json([
-//            'message' => $request->query->get('page'),
-////            'path' => 'src/Controller/HomeScreen2Controller.php',
-//        ]);
-//    }
+    #[Route('/homepage', name: 'homepage', methods: ['GET'])]
+    public function index(Request $request): Response
+    {
+        return $this->json([
+            'message' => $request->query->get('page')
+        ]);
+    }
 
     #[Route('/recipe/{id}', name: 'get_recipe', methods: ['GET'])]
     public function recipe($id, Request $request) {
         return $this->json([
             'message' => 'Requesting recipe with id' . $id,
-            'page' => $request->query->get('page'),
-//            'path' => 'src/Controller/HomeScreen2Controller.php',
+            'page' => $request->query->get('page')
         ]);
     }
 
-    #[Route('/recipes', name: 'get_all_recipes', methods: ['GET'])]
+    #[Route('/recipes', name: 'all_recipes', methods: ['GET'])]
     public function getAllRecipes() {
         $rootPath = $this->getParameter('kernel.project_dir');
         $recipes = file_get_contents($rootPath.'/resources/recipes.json');
@@ -38,22 +37,22 @@ class HomeScreen2Controller extends AbstractController
     }
 
     public function other() {
-        return $this->redirectToRoute('home_screen');
+        return $this->redirectToRoute("homepage");
     }
 
-    #[Route('/recipes/add', name: 'add_new_recipe')]
-    public function addRecipe() {
+    #[Route('/recipes/add', name: 'add_new_recipe', methods: ['GET'])]
+    public function addRecipe(Request $request) {
         $entityManager = $this->getDoctrine()->getManager();
 
         $newRecipe = new Recipe();
-        $newRecipe-> setName("Omelette");
-        $newRecipe-> setIngredients("eggs, oil");
-        $newRecipe-> setDifficulty("easy");
+        $newRecipe-> setName($request->query->get('name'));
+        $newRecipe-> setIngredients($request->query->get('ingredients'));
+        $newRecipe-> setDifficulty($request->query->get('difficulty'));
 
         $entityManager->persist($newRecipe);
         $entityManager->flush();
 
-        return new Response("trying to add new recipe ..." . $newRecipe->getId());
+        return new Response("trying to add new recipe" . $newRecipe->getId());
     }
 
     #[Route('/recipes/all', name: 'get_all_recipes')]
@@ -70,6 +69,7 @@ class HomeScreen2Controller extends AbstractController
         }
         return $this->json($resp);
     }
+
 }
 
 
