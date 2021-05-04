052aae6e2c70f833772324741e1827b722e5b341
tna007 <anh.to@edu.bc.fi>
2021-05-04 13:39:06 +0300

    Add database

--- src/Controller/HomeScreen2Controller.php
+++ src/Controller/HomeScreen2Controller.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeScreen2Controller extends AbstractController
 {
-    #[Route('/home/screen', name: 'home_screen', methods: ['GET'])]
-    public function index(Request $request): Response
-    {
-        return $this->json([
-            'message' => $request->query->get('page'),
-//            'path' => 'src/Controller/HomeScreen2Controller.php',
-        ]);
-    }
+//    #[Route('/home/screen', name: 'home_screen', methods: ['GET'])]
+//    public function index(Request $request): Response
+//    {
+//        return $this->json([
+//            'message' => $request->query->get('page'),
+////            'path' => 'src/Controller/HomeScreen2Controller.php',
+//        ]);
+//    }
 
     #[Route('/recipe/{id}', name: 'get_recipe', methods: ['GET'])]
     public function recipe($id, Request $request) {
@@ -39,4 +40,36 @@ class HomeScreen2Controller extends AbstractController
     public function other() {
         return $this->redirectToRoute('home_screen');
     }
+
+    #[Route('/recipes/add', name: 'add_new_recipe')]
+    public function addRecipe() {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $newRecipe = new Recipe();
+        $newRecipe-> setName("Omelette");
+        $newRecipe-> setIngredients("eggs, oil");
+        $newRecipe-> setDifficulty("easy");
+
+        $entityManager->persist($newRecipe);
+        $entityManager->flush();
+
+        return new Response("trying to add new recipe ..." . $newRecipe->getId());
+    }
+
+    #[Route('/recipes/all', name: 'get_all_recipes')]
+    public function getAllRecipe() {
+        $recipes = $this->getDoctrine()->getRepository(Recipe::class)->findAll();
+
+        $resp = [];
+        foreach ($recipes as $recipe) {
+            $resp[] = array(
+                'name' => $recipe->getName(),
+                'ingredients' => $recipe->getIngredients(),
+                'difficulty' => $recipe->getDifficulty()
+            );
+        }
+        return $this->json($resp);
+    }
 }
+
+
