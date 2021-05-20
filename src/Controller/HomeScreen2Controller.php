0651b5bdee959a507ca0289f31ddd71ccafa7d15
tna007 <anh.to@edu.bc.fi>
2021-05-20 12:23:17 +0300

    Update rm route

--- src/Controller/HomeScreen2Controller.php
+++ src/Controller/HomeScreen2Controller.php
@@ -197,7 +197,7 @@ class HomeScreen2Controller extends AbstractController
     public function removeRecipe($id) {
         $entityManager = $this->getDoctrine()->getManager();
         $recipe = $this->getDoctrine()->getRepository(Recipe::class)->find($id);
-        $ingredient = $this->getDoctrine()->getRepository(Ingredient::class)->findOneBy($recipe);
+        $ingredient = $this->getDoctrine()->getRepository(Ingredient::class)->find($id);
 
         if (!$recipe) {
             throw $this->createNotFoundException(
